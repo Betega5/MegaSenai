9259db4c911a59d420fccdf1e6f406a91d029342
user <brunopbetega2gmail.com>
2023-12-12 20:01:25 -0300

    chore: commit inicial

---
+++ rev1.js
@@ -0,0 +1,23 @@
+//let sorteado
+// passando um valor, somente um sinal de " = "
+//sorteado = parseInt(Math.random()*50)
+// comparando um valor, dois sinais de " = "
+// if(sorteado % 2 == 0){
+//     console.log(`O número sorteado ${sorteado} é par.`)
+// }else{
+//     console.log(`O número sorteado ${sorteado} é ímpar.`)
+// }
+
+//*---novo---*
+
+let sorteado
+let nums = []
+// sorteado = parseInt(Math.random()*50)
+
+console.log(sorteado)
+
+for(let i = 0; i < 10; i++){
+    nums[i] = parseInt(Math.random()*50)
+}
+ 
+//console.log(nums.sort((a, b) => a -b)) 
